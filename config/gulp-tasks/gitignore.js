08faa52161c4b55525d50f41cced1ea8c5a7b772
okotovenko <oakotovenko@ukr.net>
2022-09-17 19:45:39 +0300

    2

--- config/gulp-tasks/gitignore.js
+++ config/gulp-tasks/gitignore.js
@@ -8,10 +8,10 @@ export const gitignore = () => {
 		fs.appendFile('./.gitignore', 'flsStartTemplate/\r\n', cb);
 		fs.appendFile('./.gitignore', 'node_modules/\r\n', cb);
 		fs.appendFile('./.gitignore', '.gitignore\r\n', cb);
-		fs.appendFile('./.gitignore', 'dist/\r\n', cb);
+		// fs.appendFile('./.gitignore', 'dist/\r\n', cb);
 		fs.appendFile('./.gitignore', 'Source/\r\n', cb);
 		fs.appendFile('./.gitignore', 'version.json\r\n', cb);
-		// fs.appendFile('./.gitignore', app.buildFolder + '\r\n', cb);
+		fs.appendFile('./.gitignore', app.buildFolder + '\r\n', cb);
 		fs.appendFile('./.gitignore', '**/*.zip\r\n', cb);
 		fs.appendFile('./.gitignore', '**/*.rar\r\n', cb);
 		//if (projectName !== 'flsStart') del('./.git/');
